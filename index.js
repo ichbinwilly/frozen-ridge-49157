e9c2ca2806c84f889a80a8dc47413e03ffd52f3f
ichbinwilly <mibhpwosiay@gmail.com>
2018-10-28 20:49:38 +0800

    npm init and create emit example

---
+++ index.js
@@ -0,0 +1,9 @@
+//event.js ¤å¥ó
+var EventEmitter = require('events').EventEmitter; 
+var event = new EventEmitter(); 
+event.on('some_event', function() { 
+    console.log('some_event triggered'); 
+}); 
+setTimeout(function() { 
+    event.emit('some_event'); 
+}, 5000); 
